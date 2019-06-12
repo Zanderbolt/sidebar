3d265d8ac7eac8a3204419704ceb5d113cdea4ab
Christian <christian_renteria@ucol.mx>
2019-06-12 13:59:20 -0500

    Basic Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show:boolean = false;
   title = 'SGAglacial';
+
+  onToggleSidebar() {
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("working-area").classList.toggle('active');  
+    if (this.show)
+      this.show = false
+    else if (!this.show)
+      this.show = true;
+  }
+
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// fontAwesomeModule
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    // Add an icon to the library for convenient access in other components
+    library.add(fas, far)
+  }
+}
